15e3b70d5e69dab76fd618680d843675934c8db1
Lucas Marciano <lucasmarciano@mobister.com.br>
2015-05-18 08:55:40 -0300

    Primeiro Commit

---
+++ aula05.module.js
@@ -0,0 +1,10 @@
+//Criando um modulo
+//[] inserir as dependencias do modulo
+var appSaudacao = angular.module('appSaudacao',[]);
+
+//controlers
+appSaudacao.filter('ola', function(){
+    return function(nome){
+        return "Olá" + nome;
+    }
+});
